23de0dfcc5a9d582b9176906b8b14dc16ec53fb2
Tom Oinn <tomoinn@gmail.com>
2012-05-10 00:38:09 +0100

    Made listening port for server configurable with --port

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var app = express.createServer();
+var args = require('optimist')
+	.usage('Usage: $0 --port [num]')
+	.default("port", 8080)
+	.alias('p', 'port')
+	.describe('p', 'The port number on which the server should listen for connections.').argv;
 
 app.configure(function() {
 	app.use(express.bodyParser());
@@ -13,15 +18,12 @@ app.configure(function() {
 });
 
 var fs = require('fs');
-var dummyData = JSON.parse(fs.readFileSync(__dirname
-		+ "/public/mock-data/test-data.json"));
+var dummyData = JSON.parse(fs.readFileSync(__dirname + "/public/mock-data/test-data.json"));
 
 console.log("Loaded test data.");
 
 var overlapsRange = function(startA, endA, startB, endB) {
-	return ((startA <= startB && endA >= endB)
-			|| (startB <= startA && endB >= endA)
-			|| (startB <= startA && endB >= startA) || (startB <= endA && endB >= endA));
+	return (endB > startA) && (startB < endA);
 };
 
 var clamp = function(min, max, value) {
@@ -30,8 +32,8 @@ var clamp = function(min, max, value) {
 
 // Check that we can intercept routes
 app.get("/api/text/:textid/:start/:end", function(req, res) {
-	console.log("Request for text chunk - {textid:" + req.params.textid
-			+ ", start:" + req.params.start + ", end:" + req.params.end + "}");
+	// console.log("Request for text chunk - {textid:" + req.params.textid
+	// + ", start:" + req.params.start + ", end:" + req.params.end + "}");
 	var start = clamp(0, dummyData.text.length, parseInt(req.params.start));
 	var end = clamp(0, dummyData.text.length, parseInt(req.params.end));
 	var includeAnnotation = function(annotation) {
@@ -44,11 +46,11 @@ app.get("/api/text/:textid/:start/:end", function(req, res) {
 		typography : dummyData.typography.filter(includeAnnotation),
 		semantics : dummyData.semantics.filter(includeAnnotation)
 	};
-//	console.log("Returning data :");
-//	console.log(data);
+	// console.log("Returning data :");
+	// console.log(data);
 	res.json(data);
 });
 
 // Start app on localhost:80
-app.listen(8080);
-console.log("Server running");
+app.listen(args.port);
+console.log("Textus listening on port "+args.port);
