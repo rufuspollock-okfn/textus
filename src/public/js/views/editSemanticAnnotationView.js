e686e6ab80937c01b84cda9a1f495b7b54f93800
Tom Oinn <tomoinn@gmail.com>
2012-06-28 12:55:54 +0100

    Added import tool from json

--- src/public/js/views/editSemanticAnnotationView.js
+++ src/public/js/views/editSemanticAnnotationView.js
@@ -1,67 +1,89 @@
 /**
- * UI Component to edit or create a single semantic annotation payload. Currently just supports free
- * text annotations.
+ * UI Component to edit or create a single semantic annotation payload.
+ * Currently just supports free text annotations.
  */
-define([ 'jquery', 'underscore', 'backbone', 'text!templates/editSemanticAnnotationView.html', 'form' ], function($, _,
-		Backbone, layoutTemplate, Form) {
+define([ 'jquery', 'underscore', 'backbone',
+		'text!templates/editSemanticAnnotationView.html', 'form' ], function($,
+		_, Backbone, layoutTemplate, Form) {
 
 	/**
-	 * Holds schemas used for backbone-forms and keyed on the 'type' field of the semantic
-	 * annotations. Provides edit support for the various kinds of semantic annotation payload.
+	 * Holds schemas used for backbone-forms and keyed on the 'type' field of
+	 * the semantic annotations. Provides edit support for the various kinds of
+	 * semantic annotation payload.
 	 */
 	var schemas = {
 		"textus:comment" : {
-			"text" : "TextArea"
+			name : "Comment",
+			schema : {
+				"text" : "TextArea",
+				"anotherText" : "Text"
+			}
 		},
 		"textus:bibjson" : {
-		// TODO - fix BibJSON schema here
+			name : "BibJSON",
+			schema : {
+			// TODO - fix BibJSON schema here
+			}
 		}
 	};
 
-	return Backbone.View.extend({
-
-		intialize : function() {
-			_.bindAll(this);
-
-		},
-
-		render : function() {
-
-			this.$el.html(layoutTemplate);
-			var presenter = this.options.presenter;
-			var annotation = this.options.annotation;
-			var creating = (annotation.payload == null);
-			if (creating) {
-				this.$el.find('#annotationEditorTitle').html("Create new annotation");
-			} else {
-				this.$el.find('#annotationEditorTitle').html("Edit annotation");
-			}
-			console.log(this.options);
-			if (schemas[annotation.type]) {
-				var form = new Form({
-					data : annotation.payload,
-					schema : schemas[annotation.type]
+	/**
+	 * Populate the annotation editor from a given annotation.type, i.e.
+	 * "textus:comment".
+	 */
+	var populateAnnotationEditor = function(el, annotation, presenter) {
+		if (schemas[annotation.type]) {
+			var creating = (annotation.payload==null);
+			var form = new Form({
+				data : annotation.payload,
+				schema : schemas[annotation.type].schema
+			});
+			el.find('#annotationEditor').append(form.render().el);
+			el.find('#annotationEditor').append(
+					"<button id='submitAnnotation'>"
+							+ (creating ? "Create" : "Update") + "</button>");
+			el.find('#submitAnnotation').bind("click", function() {
+				console.log(form.getValue());
+				presenter.storeAnnotation({
+					type : annotation.type,
+					payload : form.getValue()
 				});
-				this.$el.find('#annotationEditor').append(form.render().el);
-				this.$el.find('#annotationEditor').append(
-						"<button id='submitAnnotation'>" + (creating ? "Create" : "Update") + "</button>");
-				this.$el.find('#submitAnnotation').bind("click", function() {
-					console.log(form.getValue());
-					presenter.storeAnnotation({
-						type : annotation.type,
-						payload : form.getValue()
-					});
-				});
-			} else {
-				console.log("Unable to find edit schema for annotation " + annotation);
-			}
-			return this;
-		},
-
-		giveFocus : function() {
-			this.$el.find('textarea#text').focus();
+			});
+		} else {
+			console.log("Unable to find edit schema for annotation "
+					+ annotation);
 		}
+	};
 
-	});
+	return Backbone.View
+			.extend({
+
+				intialize : function() {
+					_.bindAll(this);
+				},
+
+				render : function() {
+
+					this.$el.html(layoutTemplate);
+					var presenter = this.options.presenter;
+					var annotation = this.options.annotation;
+					var creating = (annotation.payload == null);
+					if (creating) {
+						this.$el.find('#annotationEditorTitle').html(
+								"Create new annotation");
+					} else {
+						this.$el.find('#annotationEditorTitle').html(
+								"Edit annotation");
+					}
+					console.log(this.options);
+					populateAnnotationEditor(this.$el, annotation, presenter);
+					return this;
+				},
+
+				giveFocus : function() {
+					this.$el.find('input')[0].focus();
+				}
+
+			});
 
 });
---
+++ src/tools/import-json.js
@@ -0,0 +1,39 @@
+/**
+ * Import a text from a JSON document as defined in the import specification
+ */
+var fs = require('fs');
+var args = require('optimist')
+		.usage('Usage: $0 --title TITLE --file JSONFILE')
+		.describe(
+				'title',
+				'Title to assign in the generated BibJSON (just temporary for now, will be done properly later)')
+		.describe('file',
+				'A JSON file containing text, typography, semantics and structure')
+		.demand([ 'title', 'file' ]).argv;
+// Textus configuration
+var conf = require('../js/textusConfiguration.js').conf();
+// Backend datastore, requires configuration
+var ds = require('../js/datastore/dataStore-elastic.js')(conf);
+
+var importData = fs.readFileSync(args.filename, 'utf8');
+ds.importData(importData, function(err, textId) {
+	if (err) {
+		console.log("Error!", err);
+	} else {
+		console.log("Stored text with text ID", textId);
+		ds.storeBibliographicReferences([ {
+			'title' : args.title,
+			'textus' : {
+				role : 'text',
+				textId : textId,
+				userId : 'auto-import'
+			}
+		} ], function(err) {
+			if (err) {
+				console.log("Unable to store references", err);
+			} else {
+				console.log("Success");
+			}
+		});
+	}
+});
